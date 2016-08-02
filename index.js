2742f2aa93d25ce838b683b745dad9f0228ecdec
Addison <funk.addison@gmail.com>
2016-08-02 12:56:25 -0400

    adding calculator functionality and html

--- index.js
+++ index.js
@@ -2,5 +2,25 @@ angular.module("calcApp", []);
 angular.module("calcApp").controller("calcCtrl", function() {
 	console.log("calcCtrl is alive!");
 	// one option = $scope.inputName = 'World'; 
-	this.inputName = 'Universe!'; 
+	this.value1 = 0;
+	this.value2 = 0;
+	this.result = 0; 
+
+	this.add = function() {
+		this.result = this.value1 + this.value2;
+	}
+	this.subtract = function() {
+		this.result = this.value1 - this.value2;
+	}
+	this.multiply = function() {
+		this.result = this.value1 * this.value2;
+	}
+	this.divide = function() {
+		this.result = this.value1 / this.value2;
+	}
+	this.reset = function() {
+		this.result = 0;
+		this.value1 = 0;
+		this.value2 = 0; 
+	}
 });
